b49f1be92e060947ba3854e3758f98790bfa6cf7
atikaahussain <intjradiant@gmail.com>
2025-10-15 13:19:33 +0500

    index file restored+some changes

--- going-seventeen/src/App.js
+++ going-seventeen/src/App.js
@@ -21,7 +21,7 @@
     const [activeCategory, setActiveCategory] = useState("everything");
 
     useEffect(() => {
-      fetch("http://192.168.100.15:5000/api/playlists")
+      fetch("http://localhost:5000/api/playlists")
         .then((res) => res.json())
         .then((data) => setPlaylists(data))
         .catch((err) => console.error(err));
